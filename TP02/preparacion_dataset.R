b509bfdb57d218d735506e7f6ad1206feeebacf3
GermanFernandezz <83498105+GermanFernandezz@users.noreply.github.com>
2024-11-25 22:27:56 -0300

    Add files via upload

--- TP02/preparacion_dataset.R
+++ TP02/preparacion_dataset.R
@@ -1,8 +1,22 @@
+# Directorio de trabajo----
+setwd("/Users/jorgefernandez/Documents/Cienciadedatos/EEA2024/TP02")
+
+# Carga de datos----
 players_value <- read.csv("players.csv")
 colnames(players_value)
-players_metrics <- read.csv("premier-player-23-24.csv")
+players_metrics <- read.csv("top5-players.csv")
 colnames(players_metrics)
 
+#-------------
+
+# Seleccionarlas 5 ligas top europeas en Players Value----
+# Me quedo con los valores de los jugadores de las 5 ligas mas importantes
+ligas <- c("IT1", "L1", "GB1", "ES1", "FR1")
+players_value <- players_value[players_value$current_club_domestic_competition_id %in% ligas, ]
+
+#--------------
+
+# Saco tildes y mayúsculas de variables equipo y nombres de jugadores----
 # Función para remover tildes y caracteres adicionales
 remove_accents <- function(text) {
   # Convierte a ASCII eliminando tildes
@@ -12,56 +26,58 @@ remove_accents <- function(text) {
   return(text)
 }
 
-# Aplicar la función a la variable (columna) 'nombre' en el data frame
+# Consolido nombre de la variable nombre de jugador
+colnames(players_metrics)[colnames(players_metrics) == "Player"] <- "name"
+
+# Aplicar la función a la variable (columna) 'nombre' y "Squad" en el data frame
 players_value$name <- sapply(players_value$name, remove_accents)
-players_metrics$Player <- sapply(players_metrics$Player, remove_accents)
+players_metrics$name <- sapply(players_metrics$name, remove_accents)
+players_metrics$Squad <- sapply(players_metrics$Squad, remove_accents)
+players_value$current_club_name <- sapply(players_value$current_club_name, remove_accents)
 
 #Elimino mayúsculas
-players_metrics$Player <- tolower(players_metrics$Player)
+players_metrics$name <- tolower(players_metrics$name)
 players_value$name <- tolower(players_value$name)
+players_metrics$Squad <- tolower(players_metrics$Squad)
+players_value$current_club_name <- tolower(players_value$current_club_name)
 
+#--------------
+
+# Consolidar nombre de equipo----
 # Vector con nombres de equipos como estan en players_metrics
-unique(players_value$equipo)
-unique(players_metrics$Team)
-equipos <- unique(players_metrics$Team)
-
+equipos <- unique(players_metrics$Squad)
+equipos
+# Consolidar nombre de equipo----
 # Agrega variable nueva "equipo" a value y luego agrega nombre de equipo como está en metrics
-players_value$equipo <- NA
-
+players_value$Squad <- NA
 for (equipo in equipos){
   for (i in 1:32405) {
     if (grepl(equipo, players_value$current_club_name[i])){
-      players_value$equipo[i] <- equipo
+      players_value$Squad[i] <- equipo
   }
 }
 }
 
+unique(players_value$Squad)
 
-# Crear las variables "precio" y "precio mas alto" en metrics
-players_metrics$precio <- NA
-players_metrics$precio_mas_alto <- NA
+# Son 96 equipos en Players Metric y 83 en Players Value.
 
-# Agrega "precio" y "precio mas alto" a los datos que coinciden en nombre de jugador y equipo
-for (i in 1:580){
-  for (j in 1:32495){
-    if (!is.na(players_metrics$Player[i]) && !is.na(players_value$name[j]) &&
-        !is.na(players_metrics$Team[i]) && !is.na(players_value$equipo[j]) &&
-        players_metrics$Player[i] == players_value$name[j] &&
-        players_metrics$Team[i] == players_value$equipo[j]){
-      players_metrics$precio[i] <- players_value$market_value_in_eur[j] 
-      players_metrics$precio_mas_alto[i] <- players_value$highest_market_value_in_eur[j]
-    }
-  }
-}
+#--------------
 
-# elimina los NA. Los datos de values parecen estar mas actualizados que los de metrics
-# ej: julian alvarez en values está en At. Madrid, en metrics en el City.
-sum(is.na(players_metrics$precio))
-dataset <- na.omit(players_metrics[!is.na(players_metrics$precio), ])
+# MERGE INTERSECCION----
+# Realiza un merge para combinar ambas tablas basado en name y Squad
+merged_data <- merge(players_metrics, players_value, 
+                     by = c("name", "Squad"), 
+                     all = FALSE)  # Preserva todas las filas de players_metrics
 
-# Verifico que no haya mas NA
-sum(is.na(dataset$precio))
-sum(is.na(dataset$precio_mas_alto))
+# Eliminar los datos que no tiene precio de jugador
+merged_data <- merged_data[!is.na(merged_data$market_value_in_eur),]
+
+#Algunos grafiquitos rápidos
+boxplot(merged_data$market_value_in_eur)
+plot(merged_data$Gls, merged_data$market_value_in_eur)
+plot(merged_data$Age, merged_data$market_value_in_eur)
+
 
 # Escribo el dataset
-write.csv(dataset, "dataset.csv", row.names = FALSE)
+write.csv(merge_data, "dataset.csv", row.names = FALSE)
